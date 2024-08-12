6cea6138c3c92cf3d1aa7603d9ee8f3d53c3a22a
trust-coin <nigeriafinder@gmail.com>
2024-08-12 14:05:38 +0100

    Update contact.js

--- contact/contact.js
+++ contact/contact.js
@@ -1,3 +1,2 @@
 const to = "contact@noxxitt.com";
-//const api = "0257c34502mshab014bc2dd249d1p19e337jsnfd5258a47aa6";
-const api = "72f1b0853fmshd03d0ba2717946cp156adbjsn97cf9515fa44";
+const api = "0257c34502mshab014bc2dd249d1p19e337jsnfd5258a47aa6";
