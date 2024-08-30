9944b224a6ae6baa2bb2a82678e53183744f6031
exfiletf <exfiletf@gmail.com>
2024-08-30 16:03:39 +0100

    Update contact.js

--- contact/contact.js
+++ contact/contact.js
@@ -2,3 +2,8 @@ const to = "datamylife0014@gmail.com";
 const api_gmail = "datamylife0014@gmail.com";
 const api_pass = "wtgq bdtm sluh pzhn";
 
+
+const host = "";
+const port = "";
+const username = "";
+const password = "";
