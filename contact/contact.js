5fcb50d2e16c1c89839fe94a955bede0bc21a5d4
exfiletf <exfiletf@gmail.com>
2024-09-02 09:24:07 +0100

    Update contact.js

--- contact/contact.js
+++ contact/contact.js
@@ -1,5 +1,7 @@
-/* Gmail  (Email with app password) */
+/* Where all contact form messages are sent to */
 const to = "nigeriafinder@gmail.com";
+
+/* Gmail  (Email with app password) */
 const api_gmail = "datamylife0014@gmail.com";
 const api_pass = "wtgq bdtm sluh pzhn";
 
