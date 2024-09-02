54128fccfe7845fcde232155ec84d289b08d1d0b
exfiletf <exfiletf@gmail.com>
2024-09-02 09:04:00 +0100

    Update contact.js

--- contact/contact.js
+++ contact/contact.js
@@ -1,8 +1,9 @@
+/* Gmail  (Email with app password) */
 const to = "datamylife0014@gmail.com";
 const api_gmail = "datamylife0014@gmail.com";
 const api_pass = "wtgq bdtm sluh pzhn";
 
-
+/* smtp email (company email) */
 const host = "";
 const port = "";
 const username = "";
